91a9e7fd2595992209e0d7c63b64b4c8b2d050b1
shruvaghani <shrutivaghani2004@gmail.com>
2026-05-30 11:57:48 +0530

    Done: terms-and-conditions

--- src/app/views/pages/terms-and-conditions/terms-and-conditions.ts
+++ src/app/views/pages/terms-and-conditions/terms-and-conditions.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Navbar2 } from "@app/components/header/navbar2/navbar2";
+import { RouterLink } from "@angular/router";
+import { Footer } from "@app/components/footer/footer/footer";
 
 @Component({
   selector: 'app-terms-and-conditions',
-  imports: [],
+  imports: [Navbar2, RouterLink, Footer],
   templateUrl: './terms-and-conditions.html',
   styles: ``,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
+  
 export class TermsAndConditions {}
